8f2d113312aa03673a8a3ee05531db25c70c4e83
Swanand Vaidya <ssvaidya28@gmail.com>
2022-02-20 16:45:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/swanandvaidya/pawlovers/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.swanandvaidya.pawlovers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
